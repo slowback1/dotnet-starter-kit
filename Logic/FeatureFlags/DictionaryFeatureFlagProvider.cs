d1a751fc8bd40097ed715b47fe9d17eacf034012
andrew <dwobeck@gmail.com>
2025-10-03 20:48:32 -0500

    Port over feature flags functionality from the dotnet toolkit

---
+++ Logic/FeatureFlags/DictionaryFeatureFlagProvider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Interfaces;
+using Common.Models;
+
+namespace Logic.FeatureFlags;
+
+public class DictionaryFeatureFlagProvider : IFeatureFlagProvider
+{
+    private readonly Dictionary<string, bool> _featureFlags;
+
+    public DictionaryFeatureFlagProvider(Dictionary<string, bool> featureFlags)
+    {
+        _featureFlags = featureFlags;
+    }
+
+    public async Task<IEnumerable<FeatureFlag>> GetFeatureFlags()
+    {
+        return _featureFlags.Select(kvp => new FeatureFlag
+        {
+            Name = kvp.Key,
+            IsEnabled = kvp.Value
+        });
+    }
+}
